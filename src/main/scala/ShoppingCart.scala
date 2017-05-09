63c017144b2106d1fa20ae47a42d4d447b297d59
Chris Boucher <caboucher1993@gmail.com>
2017-05-09 19:56:02 +0100

    Added tests for step 2 and unimplemented functions

--- src/main/scala/ShoppingCart.scala
+++ src/main/scala/ShoppingCart.scala
@@ -1,6 +1,10 @@
 
 class ShoppingCart {
 
+  def calculateAppleDiscount(items: Seq[Item]): Double = ???
+
+  def calculateOrangeDiscount(items: Seq[Item]): Double = ???
+
   def checkout(items: Seq[Item]): Double = items.map(_.price).sum
 
 }
--- src/test/scala/ShoppingCartSpec.scala
+++ src/test/scala/ShoppingCartSpec.scala
@@ -12,11 +12,109 @@ class ShoppingCartSpec extends WordSpec with MustMatchers with GeneratorDrivenPr
 
   implicit val arbCheckoutGen: Arbitrary[Seq[Item]] = Arbitrary(checkoutGen)
 
+  "calculateAppleDiscount" must {
+
+    "calculate the total discount to be 0 when 0 apples are in the basket" in {
+      val classUnderTest: ShoppingCart = new ShoppingCart()
+      val input: Seq[Item] = Seq()
+      val result: Double = classUnderTest.calculateAppleDiscount(input)
+      val expectedResult: Double = 0D
+
+      result mustBe expectedResult
+    }
+
+    "calculate the total discount to be 0 when 1 apple is in the basket" in {
+      val classUnderTest: ShoppingCart = new ShoppingCart()
+      val input: Seq[Item] = Seq(Apple)
+      val result: Double = classUnderTest.calculateAppleDiscount(input)
+      val expectedResult: Double = 0D
+
+      result mustBe expectedResult
+    }
+
+    "calculate the total discount to be 0.6 when 2 apples are in the basket" in {
+      val classUnderTest: ShoppingCart = new ShoppingCart()
+      val input: Seq[Item] = Seq(Apple, Apple)
+      val result: Double = classUnderTest.calculateAppleDiscount(input)
+      val expectedResult: Double = 0.6D
+
+      result mustBe expectedResult
+    }
+
+    "calculate the total discount to be 1.8 when 6 apples are in the basket" in {
+      val classUnderTest: ShoppingCart = new ShoppingCart()
+      val input: Seq[Item] = Seq(Apple, Apple, Apple, Apple, Apple, Apple)
+      val result: Double = classUnderTest.calculateAppleDiscount(input)
+      val expectedResult: Double = 1.8D
+
+      result mustBe expectedResult
+    }
+
+    "calculate the total discount to be 1.8 when 7 apples are in the basket" in {
+      val classUnderTest: ShoppingCart = new ShoppingCart()
+      val input: Seq[Item] = Seq(Apple, Apple, Apple, Apple, Apple, Apple, Apple)
+      val result: Double = classUnderTest.calculateAppleDiscount(input)
+      val expectedResult: Double = 1.8D
+
+      result mustBe expectedResult
+    }
+
+  }
+
+  "calculateOrangeDiscount" must {
+
+    "calculate the total discount to be 0 when 0 oranges are in the basket" in {
+      val classUnderTest: ShoppingCart = new ShoppingCart()
+      val input: Seq[Item] = Seq()
+      val result: Double = classUnderTest.calculateOrangeDiscount(input)
+      val expectedResult: Double = 0D
+
+      result mustBe expectedResult
+    }
+
+    "calculate the total discount to be 0 when 2 oranges are in the basket" in {
+      val classUnderTest: ShoppingCart = new ShoppingCart()
+      val input: Seq[Item] = Seq(Orange, Orange)
+      val result: Double = classUnderTest.calculateOrangeDiscount(input)
+      val expectedResult: Double = 0D
+
+      result mustBe expectedResult
+    }
+
+    "calculate the total discount to be 0.25 when 3 oranges are in the basket" in {
+      val classUnderTest: ShoppingCart = new ShoppingCart()
+      val input: Seq[Item] = Seq(Orange, Orange, Orange)
+      val result: Double = classUnderTest.calculateOrangeDiscount(input)
+      val expectedResult: Double = 0.25D
+
+      result mustBe expectedResult
+    }
+
+    "calculate the total discount to be 0.5 when 6 oranges are in the basket" in {
+      val classUnderTest: ShoppingCart = new ShoppingCart()
+      val input: Seq[Item] = Seq(Orange, Orange, Orange, Orange, Orange, Orange)
+      val result: Double = classUnderTest.calculateOrangeDiscount(input)
+      val expectedResult: Double = 0.5D
+
+      result mustBe expectedResult
+    }
+
+    "calculate the total discount to be 0.5 when 7 oranges are in the basket" in {
+      val classUnderTest: ShoppingCart = new ShoppingCart()
+      val input: Seq[Item] = Seq(Orange, Orange, Orange, Orange, Orange, Orange)
+      val result: Double = classUnderTest.calculateOrangeDiscount(input)
+      val expectedResult: Double = 0.5D
+
+      result mustBe expectedResult
+    }
+
+  }
+
   "checkout" must {
 
     "calculate the total price of 3 Apples and 1 Orange to 2.05" in {
       val classUnderTest: ShoppingCart = new ShoppingCart()
-      val input = Seq(Apple, Apple, Apple, Orange)
+      val input: Seq[Item] = Seq(Apple, Apple, Apple, Orange)
       val result: Double = classUnderTest.checkout(input)
       val expectedResult: Double = 2.05D
 
@@ -25,7 +123,7 @@ class ShoppingCartSpec extends WordSpec with MustMatchers with GeneratorDrivenPr
 
     "calculate the total price of 1 Apple and 3 Oranges to 1.35" in {
       val classUnderTest: ShoppingCart = new ShoppingCart()
-      val input = Seq(Apple, Orange, Orange, Orange)
+      val input: Seq[Item] = Seq(Apple, Orange, Orange, Orange)
       val result: Double = classUnderTest.checkout(input)
       val expectedResult: Double = 0.6D + 0.25D + 0.25D + 0.25D
 
